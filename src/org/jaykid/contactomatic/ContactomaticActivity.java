11dae5b63c65b57b5b7230cb1e02971029f210ba
Jay Kid <cyph3rfox@gmail.com>
2012-04-15 18:45:28 +0200

    Dialer tab working. Design remake pending

--- src/org/jaykid/contactomatic/ContactomaticActivity.java
+++ src/org/jaykid/contactomatic/ContactomaticActivity.java
@@ -1,7 +1,5 @@
 package org.jaykid.contactomatic;
 
-
-
 import android.app.TabActivity;
 import android.content.Intent;
 import android.content.res.Resources;
--- src/org/jaykid/contactomatic/PhoneDialerActivity.java
+++ src/org/jaykid/contactomatic/PhoneDialerActivity.java
@@ -1,26 +1,99 @@
 package org.jaykid.contactomatic;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class PhoneDialerActivity extends Activity {
+public class PhoneDialerActivity extends Activity
+{
+	private static final String PHONE_PREFIX = "tel:";
+	private static final Uri EMPTY_URI = Uri.parse(PHONE_PREFIX);
 	
 	public void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.dialpad);
-        
-//        try {
-//        	Intent callIntent = new Intent(Intent.ACTION_CALL);
-//            callIntent.setData(Uri.parse("tel:+436641234567"));
-//            startActivity(callIntent);
-//    	} catch (Exception e) {
-//    	   Log.e("Contact-o-matic", "Failed to invoke call", e);
-//    	}
-        
-        
-
-        
     }
 	
+	public void addButtonTextToDialer(View button)
+	{
+		Button uiButton = (Button)button;
+	    String buttonText = uiButton.getText().toString();
+		addCharacterToDialer(buttonText);
+	}
+	
+	private void addCharacterToDialer(String character)
+	{
+		String dialedPhoneNumberFromUI, dialedPhoneNumberFromUIWithAddedCharacter;
+		dialedPhoneNumberFromUI = getDialedPhoneNumberFromUI();
+		dialedPhoneNumberFromUIWithAddedCharacter = dialedPhoneNumberFromUI.concat(character);
+		setDialedPhoneNumber(dialedPhoneNumberFromUIWithAddedCharacter);
+	}
+	
+	public void eraseCharacterFromDialer(View button) 
+    {
+		String dialedTelephonedNumberFromUI;
+		dialedTelephonedNumberFromUI = getDialedPhoneNumberFromUI();
+		if (!dialedTelephonedNumberFromUI.isEmpty())
+		{
+			String dialedTelephonedNumberFromUIWithoutLastNumber = 
+					substractLastNumber(dialedTelephonedNumberFromUI);
+			setDialedPhoneNumber(dialedTelephonedNumberFromUIWithoutLastNumber);
+		}
+    }
+	
+	
+	private void setDialedPhoneNumber(String telephoneNumber) 
+	{
+		((TextView) findViewById(R.id.dialedNumber)).setText(telephoneNumber);
+	}
+
+	private String substractLastNumber(String dialedTelephonedNumberFromUI) 
+	{
+		String dialedTelephonedNumberFromUIWithoutLastNumber;
+		dialedTelephonedNumberFromUIWithoutLastNumber = 
+				(String) dialedTelephonedNumberFromUI.subSequence(
+						0, dialedTelephonedNumberFromUI.length()-1); 
+		return dialedTelephonedNumberFromUIWithoutLastNumber;
+	}
+
+	public void callDialedNumber(View button) 
+    {
+		Uri telephoneNumberToBeCalled = getDialedTelephoneNumber();
+		if (isAValidUri(telephoneNumberToBeCalled)) {
+			Intent callIntent = new Intent(Intent.ACTION_CALL);
+			callIntent.setData(telephoneNumberToBeCalled);
+			startActivity(callIntent);
+
+		}
+		
+    }
+
+	private Uri getDialedTelephoneNumber() 
+	{
+		Uri dialedTelephonedNumber;
+		String dialedTelephonedNumberFromUI, dialedTelephonedNumberFromUIWithPrefix;
+		dialedTelephonedNumberFromUI = getDialedPhoneNumberFromUI();
+		dialedTelephonedNumberFromUIWithPrefix = addPhonePrefix(dialedTelephonedNumberFromUI);
+		dialedTelephonedNumber = Uri.parse(dialedTelephonedNumberFromUIWithPrefix);
+		return dialedTelephonedNumber;
+	}
+
+	private String getDialedPhoneNumberFromUI() {
+		return ((TextView) findViewById(R.id.dialedNumber)).getText().toString();
+	}
+
+	private String addPhonePrefix(String dialedTelephonedNumberFromUI) {
+		return PHONE_PREFIX.concat(dialedTelephonedNumberFromUI);
+	}
+	
+	private boolean isAValidUri(Uri telephoneNumberToBeCalled)
+	{
+		if (!telephoneNumberToBeCalled.equals(EMPTY_URI)) return true;
+		return false;
+	}
 }
